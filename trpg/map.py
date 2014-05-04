749f56d8811c6fd848124a44a957e1e3aaeeae4c
TheSpeks <thespeks@users.noreply.github.com>
2014-05-04 13:51:04 -0400

    Create map.py

---
+++ trpg/map.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+def get_new_blank_map(size_x, size_y, empty='.'):
+    """Return a new blank map."""
+    if (size_x and size_y) > 1:
+        x = join([empty]*size_x)
+        return tuple(x for i in range(size_y)
+
+def build_coords_dict(map_matrix, empty='.'):
+    
+    d = {}
+    for y in matrix:
+          for i in y:
+              if i != empty:
+                  d[y.index(i), matrix.index(y)] = y[y.index(i)]
+    return d
